cf0fa1030bdd7653ae7d11b2afe4a56523ec0d5f
Daniel Fontalvo <danielfon1999@gmail.com>
2024-12-07 02:01:33 -0500

    inicializando proyecto

---
+++ src/main/java/com/gestion/ikbo_inventario/IkboInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.ikbo_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkboInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkboInventarioApplication.class, args);
+	}
+
+}
